498ae646d6c7881621ecb7ed801b8d23ee3f38e9
El-Chapo0133 <levequeloris66@gmail.com>
2024-09-06 11:22:25 +0200

    Pushed last changes

--- redirection.h
+++ redirection.h
@@ -5,19 +5,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-/**
- * struct RedirectTo - define the redirection from and to
- * if NULL, no need to redirect
- *
- * @to: redirection for stdout
- * @from: redirection for stdin
- */
-typedef struct RedirectTo
-{
-	char *to;
-	char *from;
-}
-
 void handle_redirections(char **cmd);
 
 #endif
