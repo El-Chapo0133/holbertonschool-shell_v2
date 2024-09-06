69181f8e5ecf6362b056e2c4837fe16cc9e5cfa7
El-Chapo0133 <levequeloris66@gmail.com>
2024-09-06 11:24:27 +0200

    Pushed last changes

--- redirection.c
+++ redirection.c
@@ -11,14 +11,14 @@ void handle_redirections(char **cmd)
 	int index, fd;
 	for (index = 0; cmd[index] != NULL; index++)
 	{
-		if (cmd[index] == ">" && cmd[index + 1] != NULL)
+		if (_strcmp(cmd[index], ">") == 0 && cmd[index + 1] != NULL)
 		{
 			fd = open(cmd[index + 1], 0_CREAT);
 			dup2(fd, 1);
 			close(fd);
 			index++;
 		}
-		else if (cmd[index] == "|" && cmd[index + 1] != NULL)
+		else if (_strcmp(cmd[index], "|") == 0 && cmd[index + 1] != NULL)
 		{
 			fd = open(cmd[index + 1], 0_RDONLY);
 			dup2(fd, 0);
